2e2b624d19a317e18569e27c3e938ce7a78229fb
Juan-Cruz-P <66924891+Juan-Cruz-P@users.noreply.github.com>
2022-06-04 17:39:30 -0300

    Corregir el promedio y gasto

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-function agregar() {
-
-    $('#nombre-a').empty();
-    $('#gasto-a').empty();
-    let valor1 = $('#nombre').val();
-    let lista1 = valor1.split(',');
-
-    let valor2 = $('#gasto').val();
-    let lista2 = valor2.split(',');
-
-    for (let i = 0; i < lista1.length; i++) {
-        let textoResultante1 = ` ${lista1[i]}`; // lo que se va a agregar al html
-        $('#nombre-a').append(textoResultante1); 
-
-        let textoResultante2 = ` ${lista2[i]}`; // lo que se va a agregar al html
-        $('#gasto-a').append(textoResultante2); 
-    }
+function agregar(id1, id2) {
+    let valor = $('#'+id2).val();
+    let correjido = valor.split(',');
+    let Resultado = `<p>${correjido}</p>`; // lo que se va a agregar al html
+    $('#'+id1).append(Resultado);
 }
+
+let integrantes = []; // guardo valores de integrantes aca
+
+function calcular(valores, donde1, donde2){
+    let valor = $('#'+valores).val();     // Obtengo el valor del input
+    integrantes.push(valor); // Agrego valores a lista de integrantes
+    let suma = 0; // suma de integrantes inicial
+    let promedio = 0;
+
+    // Calculos
+    for (let i = 0; i < integrantes.length; i++) {
+        suma += (parseFloat(integrantes[i])); // realizo suma de integrantes
+        promedio = suma / integrantes.length;
+        console.log(suma);
+    }
+
+    // Para html
+    let Suma = `<p>$${suma}</p>`;
+    let Promedio = `<p>$${promedio}</p>`;
+
+    // Agregado a su lugar correspondiente
+    $('#'+donde1).append(Suma);
+    $('#'+donde2).append(Promedio);
+}
+
+
+// function calcular(valores, donde){
+//     let valor = $('#'+valores).val();     // Obtengo el valor del input
+//     integrantes.push(valor); // Agrego valores a lista de integrantes
+//     let suma = 0; // suma de integrantes inicial
+
+//     // Calculos
+//     for (let i = 0; i < integrantes.length; i++) {
+//         suma += (parseFloat(integrantes[i])); // realizo suma de integrantes
+//         let promedio = suma / integrantes.length;
+//         console.log(suma);
+//     }
+
+//     // Para html
+//     let Suma = `<p>Gasto total: ${suma}</p>`;
+//     let Promedio = `<p>Promedio: ${promedio}</p>`;
+
+//     // Agregado a su lugar correspondiente
+//     $('#'+donde).append(Suma);
+//     $('#'+donde).append(Promedio);
+// }
